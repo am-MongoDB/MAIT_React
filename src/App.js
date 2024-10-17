6e63c4d5474274906be440edbda16877953aca16
am-MongoDB <andrew.morgan@mongodb.com>
2024-10-17 14:06:48 +0100

    Select role

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+import PickRole from './components/PickRole';
 import './css/App.css';
 
 function App() {
+  const [role, setRole] = useState('tbd');
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>MAIT</h1>
-      </header>
+      {/* <header className="App-header"> */}
+        {role === 'tbd' && <h1>Welcome to the MongoDB Audience Interaction Tool (MAIT)</h1>}
+      {/* </header> */}
+        {role === 'tbd' && <PickRole setRole={setRole}/>}
     </div>
   );
 }
