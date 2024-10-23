148e68260939d3e395dce13b76b2cab78b028279
am-MongoDB <andrew.morgan@mongodb.com>
2024-10-23 10:38:42 +0100

    Have option/answer components include the session ID so that they don't get reused between sessions

--- src/components/AnswerButton.jsx
+++ src/components/AnswerButton.jsx
@@ -1,36 +1,26 @@
-// import { useContext } from 'react';
-// import { UserContext } from '../../App';
 import DigitalCounter from "./counters/DigitalCounter";
 
 export default function AnswerButton({ 
-  optionNumber, // The number of this option amongst the available options
-  optionText,   // The text associated with this option
-  // allowMultipleSelections,// TODO: support multi-selection quiz
-  optionPicked, // The number of the option that the user has selected.
-  // Should benull when no options have been selected yet.
+  optionNumber,
+  optionText,
+  optionPicked,
   responseCount,
-  setOptionPicked // Function for this component to update the parent's state
-  // with the number of the selected option (this one) 
-}) {
-  // const { role } = useContext(UserContext);
-  
+  setOptionPicked
+}) { 
   function pickMe() {
-    // TODO: Enable once participant logic has been added
-    // if (role === 'participant') {
       if (!optionPicked) { setOptionPicked(optionNumber); }
-    // }
   }
   
   return (
     <div className="answer-button">
-    <button 
-    type="submit" 
-    className={`btn btn-primary btn-primary-branded btn-role ${optionPicked && optionPicked === optionNumber ? "selected-option" : ""} ${optionPicked && optionPicked !== optionNumber ? "discarded-option" : ""}`}
-    onClick={pickMe}
-    >
-    {optionText}
-    </button>
-    <DigitalCounter count={responseCount}/>
+      <button 
+        type="submit" 
+        className={`btn btn-primary btn-primary-branded btn-role ${optionPicked && optionPicked === optionNumber ? "selected-option" : ""} ${optionPicked && optionPicked !== optionNumber ? "discarded-option" : ""}`}
+        onClick={pickMe}
+        >
+        {optionText}
+      </button>
+      <DigitalCounter count={responseCount}/>
     </div>
   );
 }
--- src/components/AnswerButtonList.jsx
+++ src/components/AnswerButtonList.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+// import { useContext } from 'react';
+// import { UserContext } from '../../App';
 import AnswerButton from './AnswerButton';
 
 export default function AnswerButtonList({ 
@@ -7,23 +9,31 @@ export default function AnswerButtonList({
     options,
     responseCounts 
 }) {
+    // const { role } = useContext(UserContext);
+
     const [selectedOptions, setSelectedOptions] = 
         useState(cardType === "PickOne" ? null : []);
 
     function optionSelected(option) {
+    // TODO: Enable once participant logic has been added
+    // if (role === 'participant') {
         if (cardType === "PickOne") {
             setSelectedOptions(option)
         } else {
             setSelectedOptions(prevOptions => [...prevOptions, option]);
         }
     // TODO: Send response to BE
+    // }
+
     }
 
     return (
         <div className='responsive-grid'>
             {options.map((optionText, index) => (
                 <AnswerButton
-                    key={index + 1}
+                    // Not just using key=index so that the component doesn't
+                    // get reused between sessions
+                    key={`${sessionId}-${index}`}
                     optionNumber={index + 1}  
                     optionText={optionText}
                     optionPicked={selectedOptions}
