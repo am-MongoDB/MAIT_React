d0a6567d19c1dc14f9bd983f00a39834fcd887fd
am-MongoDB <andrew.morgan@mongodb.com>
2024-10-23 09:43:50 +0100

    Add role to global UserContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import PickRole from './components/PickRole';
 import Host from './components/Host';
 import './css/App.css';
 
+// Used for controlling host vs. participant role
+export const UserContext = createContext(null);
+
 function App() {
   const [role, setRole] = useState('tbd');
+
   return (
     <div className="App">
+      <UserContext.Provider 
+          value={
+            { role: role, setRole: setRole }
+      }>
         {role === 'tbd' && <h1>Welcome to the MongoDB Audience Interaction Tool (MAIT)</h1>}
         {role === 'tbd' && <PickRole setRole={setRole}/>}
         {role === 'host' && <Host/>}
-
+      </UserContext.Provider>
     </div>
   );
 }
--- src/components/AnswerButton.jsx
+++ src/components/AnswerButton.jsx
@@ -1,31 +1,36 @@
+// import { useContext } from 'react';
+// import { UserContext } from '../../App';
 import DigitalCounter from "./counters/DigitalCounter";
 
 export default function AnswerButton({ 
-    optionNumber, // The number of this option amongst the available options
-    optionText,   // The text associated with this option
-    // allowMultipleSelections,// TODO: support multi-selection quiz
-    optionPicked, // The number of the option that the user has selected.
-                  // Should benull when no options have been selected yet.
-    responseCount,
-    setOptionPicked // Function for this component to update the parent's state
-                    // with the number of the selected option (this one) 
+  optionNumber, // The number of this option amongst the available options
+  optionText,   // The text associated with this option
+  // allowMultipleSelections,// TODO: support multi-selection quiz
+  optionPicked, // The number of the option that the user has selected.
+  // Should benull when no options have been selected yet.
+  responseCount,
+  setOptionPicked // Function for this component to update the parent's state
+  // with the number of the selected option (this one) 
 }) {
-    function pickMe() {
-        if (!optionPicked) {
-            setOptionPicked(optionNumber);
-        }
-    }
-
-    return (
-        <div className="answer-button">
-            <button 
-                type="submit" 
-                className={`btn btn-primary btn-primary-branded btn-role ${optionPicked && optionPicked === optionNumber ? "selected-option" : ""} ${optionPicked && optionPicked !== optionNumber ? "discarded-option" : ""}`}
-                onClick={pickMe}
-            >
-                {optionText}
-            </button>
-            <DigitalCounter count={responseCount}/>
-        </div>
-    );
+  // const { role } = useContext(UserContext);
+  
+  function pickMe() {
+    // TODO: Enable once participant logic has been added
+    // if (role === 'participant') {
+      if (!optionPicked) { setOptionPicked(optionNumber); }
+    // }
+  }
+  
+  return (
+    <div className="answer-button">
+    <button 
+    type="submit" 
+    className={`btn btn-primary btn-primary-branded btn-role ${optionPicked && optionPicked === optionNumber ? "selected-option" : ""} ${optionPicked && optionPicked !== optionNumber ? "discarded-option" : ""}`}
+    onClick={pickMe}
+    >
+    {optionText}
+    </button>
+    <DigitalCounter count={responseCount}/>
+    </div>
+  );
 }
